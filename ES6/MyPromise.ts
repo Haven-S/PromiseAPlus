0b1b1c4b1ec058d2d988a14944ab33c0857f9a9e
Havens <8522649+habro@user.noreply.gitee.commkdir>
2023-10-03 16:34:19 +0800

    5. ES6

--- ES6/MyPromise.ts
+++ ES6/MyPromise.ts
@@ -103,11 +103,3 @@ MyPromise.race = function (promises) {
   });
   return newPromise;
 };
-
-MyPromise.all([
-  new MyPromise(resolve => setTimeout(() => resolve(1), 3000)), // 1
-  new MyPromise(resolve => setTimeout(() => resolve(2), 2000)), // 2
-  new Promise(resolve => setTimeout(() => resolve(3), 1000)) // 3
-]).then((res: any[]) => {
-  console.log(res);
-});
